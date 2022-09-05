1654d424eb946180d86e6636c8ab931a06db48da
Ikhlaq Firdous <ikhlaqfirdous@Ikhlaqs-MacBook-Pro.local>
2022-09-05 11:25:04 +0530

    token replaces

--- services/http-common.ts
+++ services/http-common.ts
@@ -5,6 +5,6 @@ export default axios.create({
     "Content-type": "application/json",
     Authorization:
       "Bearer " +
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlraGxhcTIwMUBnbWFpbC5jb20iLCJnaXRodWIiOiJodHRwczovL2dpdGh1Yi5jb20vaWtobGFxMSIsImlhdCI6MTY2MTg1MjkyOCwiZXhwIjoxNjYyMjg0OTI4fQ.dXZgLvJb-bl1ttDLltgTiUrb9yfjnqZlvMjy59Rjnmg",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtoYW5pa2hsYXE5OUBnbWFpbC5jb20iLCJnaXRodWIiOiJodHRwczovL2dpdGh1Yi5jb20vaWtobGFxMSIsImlhdCI6MTY2MjM1NzI0NywiZXhwIjoxNjYyNzg5MjQ3fQ.4VEGAoVCZESrL7krc9-liAvESZEQBffrtDNrALwTvyA",
   },
 });
